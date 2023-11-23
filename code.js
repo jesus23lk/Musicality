3182add7be3c41c87fa9d6f45bde38d66574463d
Jesus Garcia <jesus23lk@gmail.com>
2023-11-23 15:06:28 -0800

    first-commit

---
+++ code.js
@@ -0,0 +1,87 @@
+const notes = ['F', 'E', 'D', 'C', 'B', 'A', 'G'];
+const notesAG = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+let curLoc = '';
+
+window.onload = function() {
+
+  giveIds();
+  setupBtns();
+  curLoc = pickLocation();
+
+}
+
+function getRandom(num) {
+  return Math.floor(Math.random() * num);
+}
+
+function giveIds() {
+
+  const clef = document.getElementById('clef');
+
+  for(let i = 0; i < 9; i++) {
+
+    curChild = clef.children[i];
+    curChild.id = 'loc ' + i;
+    curChild.noteVal = notes[i % 7];
+  }
+}
+
+function setupBtns() {
+
+  const btns = document.getElementById('buttons').children;
+
+  for(let i = 0; i < btns.length; i++) {
+
+    btns[i].noteVal = notesAG[i];
+    btns[i].addEventListener('click', evaluateChoice);
+  }
+}
+
+function pickLocation() {
+  
+  const locId = getRandom(9);     //get random number 0-9
+  const loc = document.getElementById('loc ' + locId);
+
+  const note = document.createElement('img');
+  note.src = 'Images/quarter-note.png';
+  note.id = 'note';
+  loc.appendChild(note);
+
+  if(loc.className === 'line') note.style.bottom = '-30px';
+  
+  else note.style.bottom = '-7px';
+
+  return loc;
+
+}
+
+function evaluateChoice() {
+
+  const choice = this.noteVal;
+  const correctAns = curLoc.noteVal;
+  const winMessage = document.getElementById('message');
+  let winSound;
+
+  winMessage.style.display = 'grid';
+
+  if(choice === correctAns) {
+    winSound = new Audio("audio/correct-answer.wav");
+    winSound.play();
+    winMessage.style.backgroundColor = 'rgb(42, 135, 42)';
+    winMessage.textContent = 'Correct!';
+    resetClef();
+  }
+
+  else  {
+    winSound = new Audio('audio/wrong-answer.wav');
+    winSound.play();
+    winMessage.style.backgroundColor = 'rgb(198, 51, 51)';
+    winMessage.textContent = 'Try Again!';
+  }
+
+}
+
+function resetClef() {
+  curLoc.removeChild(curLoc.firstChild);
+  curLoc = pickLocation();
+}
