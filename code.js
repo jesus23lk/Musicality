99cfe5b5edb0be3862f447bee2764878036fa383
Jesus Garcia <jesus23lk@gmail.com>
2023-12-16 13:28:30 -0800

    added a lot more notes

--- code.js
+++ code.js
@@ -1,16 +1,17 @@
-const notes = ['F', 'E', 'D', 'C', 'B', 'A', 'G'];
+const notes =   ['C', 'B', 'A', 'G', 'F', 'E', 'D'];
 const notesAG = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
 
 const game = {
   noteLoc: '',
   locId: '',
-  curNote: ''
+  curNote: '',
+  locsToHide: [],
 }
 
 
 window.onload = function() {
 
-  giveIds();
+  createLocations();
   setupBtns();
   game.noteLoc = pickLocation();
 
@@ -21,17 +22,78 @@ function getRandom(num) {
   return Math.floor(Math.random() * num);
 }
 
-function giveIds() {
+function showLedgerLines(loc) {
+
+  if(loc.className === 'ledger-line') loc.style.backgroundColor = 'black';
+
+  const staff = document.getElementById('staff').children;
+  let curLoc = loc.nextSibling;
+
+  let i = 0;
+  
+  while (curLoc.idNum < 10) {
+
+    if (curLoc.className === 'ledger-line') {
+      curLoc.style.backgroundColor = 'black';
+      game.locsToHide[i] = curLoc;
+      i++;
+    }  
+
+    curLoc = curLoc.nextSibling;
+
+  }
+
+  curLoc = curLoc.previousSibling;
+  i = 0;
+
+  while (curLoc.idNum > 32) {
+
+    if (curLoc.className === 'ledger-line') {
+      curLoc.style.backgroundColor = 'black';
+      game.locsToHide[i] = curLoc;
+      i++;
+    }  
+
+    curLoc = curLoc.previousSibling;
+
+  }
+
+}
+
+function createLocations() {
 
   const clef = document.getElementById('staff');
-  const locs = clef.children;
+  
+  for(let i = 0; i < 43; i++) {
 
-  for(let i = 0; i < locs.length; i++) {
+    const curChild = document.createElement('div');
 
-    curChild = locs[i];
+    if(i % 2 !== 0) {
+
+      if(i < 11 || i > 32 || i === 21) {      //create ledger line locations
+
+        curChild.className = 'ledger-line';
+        curChild.innerHTML = `<div class = 'empty-l'></div>
+                              <div class = 'empty-r'></div>`;
+      }
+ 
+      else curChild.className = 'line';
+    }
+
+    else {
+      
+      if (i > 33 || i < 9) curChild.className = 'ledger-space';
+
+      else curChild.className = 'space';
+    }
+
     curChild.id = 'loc-' + i;
+    curChild.idNum = i;
     curChild.noteVal = notes[i % 7];
+
+    clef.appendChild(curChild);
   }
+
 }
 
 function setupBtns() {    
@@ -57,26 +119,26 @@ function pickLocation() {
   let locId;
 
   do {
-    locId = getRandom(20);
-    console.log(locId);
+    locId = getRandom(42);
   }
   while (locId === game.locId);
 
-  console.log(locId);
   game.locId = locId;
   const loc = document.getElementById('loc-' + locId);
 
-  if(loc.className === 'ledger-line') loc.style.backgroundColor = 'black';      //make ledger line visible
+  // if(loc.className === 'ledger-line') loc.style.backgroundColor = 'black';      //make ledger line visible
 
-  const note = document.createElement('img');
+  if (loc.className === 'ledger-line' || loc.className === 'ledger-space') showLedgerLines(loc);
+
+  const note = document.createElement('img');                                   //make note image visible
   game.curNote = note;
   note.src = 'Images/quarter-note1.png';
   note.id = 'note';
   loc.appendChild(note);
 
-  if(loc.className === 'line' || loc.className === 'ledger-line') note.style.bottom = '-32px';
+  if(loc.className === 'line' || loc.className === 'ledger-line') note.style.bottom = '-16.5px';
   
-  else note.style.bottom = '-23px';
+  else note.style.bottom = '-11.5px';
 
   return loc;
 
@@ -112,7 +174,17 @@ function resetClef() {
 
   game.noteLoc.removeChild(game.curNote);
 
-  if(game.locId === 10) game.noteLoc.style.backgroundColor = 'white';
+  if(game.noteLoc.className === 'ledger-line') game.noteLoc.style.backgroundColor = 'white';
+
+  if(game.locsToHide) {
+
+    for(let i = 0; i < game.locsToHide.length; i++) {
+
+      game.locsToHide[i].style.backgroundColor = 'white';
+
+    }
+
+  }
   
   game.noteLoc = pickLocation();
 
